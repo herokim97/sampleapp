f5f8d6fad95ecfd1b25666b737b857fec0c639d6
heroKim997 <ghkufud6545@gmail.com>
2024-12-27 23:04:52 +0900

    add. run-test

---
+++ src/main/kotlin/nbc/docker/sampleapp/SampleappApplication.kt
@@ -0,0 +1,11 @@
+package nbc.docker.sampleapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleappApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleappApplication>(*args)
+}
